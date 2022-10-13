457d6f3b1eb4556ca5da1f31420b8ddab8ad81b5
sutra <sutra@MSI>
2022-10-13 09:14:05 +0200

    test changement

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -65,7 +65,7 @@ static void MX_LPUART1_UART_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+//commentaire
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
